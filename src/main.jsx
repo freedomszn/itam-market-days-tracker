b132bff3e87982df7c7c47aa2043e05fb885ebf6
freedomszn <freedomwrldd@gmail.com>
2026-05-14 08:27:42 +0100

    fix: remove duplicate Analytics, add SPA rewrite

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
+// import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    {/* <Analytics /> */}
     <App />
   </React.StrictMode>,
 );
